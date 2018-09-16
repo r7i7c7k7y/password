42f25f784abf4e53c1aa217c4fb62ff56def96e3
Ricky Tsai <r7i7c7k7y@gmail.com>
2018-09-16 20:21:05 +0800

    沒機會不顯示

--- password.py
+++ password.py
@@ -1,11 +1,12 @@
 password = 'qazws015'
-n = 0
-while n < 3:
+i = 3
+while i > 0:
+	i = i -1
 	a = input('輸入密碼 :')
 	if a == password:
 		print('登入成功!')
 		break
-	else:
-		print('密碼錯誤! 還有',(2-n),'次機會!')
-	n = n + 1
+	elif i != 0:
+		print('密碼錯誤! 還有',i,'次機會!')
+	
 
